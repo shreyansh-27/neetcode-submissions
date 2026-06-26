3aa0cd3799e33fda182816d48731362710779de0
Shreyansh <114324923+shreyansh-27@users.noreply.github.com>
2026-06-26 23:40:28 +0530

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        if( len1 > len2) return false;
+
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for(int i = 0; i < len1; i++){
+            arr1[s1.charAt(i) - 'a']++;
+            arr2[s2.charAt(i) - 'a']++;
+        }
+
+        if(matches(arr1, arr2)){return true;}
+
+        for(int i = len1; i < len2; i++){
+            arr2[s2.charAt(i) -'a']++;
+
+            arr2[s2.charAt(i - len1) - 'a']--;
+
+            if(matches(arr1, arr2)) return true;
+        }
+
+        return false;
+
+
+    }
+
+    public boolean matches(int[] a1, int[] a2){
+        for(int i = 0; i < a1.length; i++){
+            if(a1[i] != a2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
